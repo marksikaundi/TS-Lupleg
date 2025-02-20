b2fcd834dc00b6d49bab2be716a5a8ce4a659767
marksikaundi <emmanuelmark117@gmail.com>
2025-02-20 11:44:05 +0200

    Add Tip component with basic structure and greeting message

---
+++ app/tips/page.tsx
@@ -0,0 +1,7 @@
+export default function Tip() {
+  return (
+    <div className="container mx-auto py-20">
+      <h2 className="text-center font-bold text-lg ">Hello there!</h2>
+    </div>
+  );
+}
