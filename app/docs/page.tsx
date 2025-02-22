c519b1692622bae941376d3a3ecf4147f47e2d3f
marksikaundi <emmanuelmark117@gmail.com>
2025-02-22 14:01:19 +0200

    CORE: Added a new page -> Docs

---
+++ app/docs/page.tsx
@@ -0,0 +1,3 @@
+export default function Docs() {
+  return <div>Hello there, welcome to the docs</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -34,9 +34,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-[#2D1637] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://www.lupleg.org/docs"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/docs"
           >
             Read our docs
           </a>
